62c9b09a8a8e55578cba5b64182393caf13c4ab7
Yuli Lanir <yuli.lanir532@gmail.com>
2026-01-03 20:51:58 +0200

    wewqe

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,8 @@
 #define SCREEN_HEIGHT 64 
 #define OLED_RESET -1 
 
+#define MOTION_PIN 18
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Koala dimensions (based on your 46-byte array)
@@ -22,18 +24,16 @@ int speedY = 1;    // How many pixels to move vertically per frame
 void setup() {
   Serial.begin(115200);
 
+  pinMode(MOTION_PIN, INPUT); // Set the PIR pin as an input
+
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); 
   }
 }
 
-void loop() {
-  // 1. Clear the old position
-  display.clearDisplay(); 
-
+void koala_motion(){
   // 2. Draw the koala at the current position
-  display.drawBitmap(0, 0, myBitmap, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
   display.drawBitmap(posX, posY, koala, KOALA_WIDTH, KOALA_HEIGHT, WHITE);
 
   // 3. Show the frame
@@ -54,4 +54,23 @@ void loop() {
 
   // Small delay to control the speed of animation
   delay(10); 
+}
+
+void loop() {
+  
+
+  display.clearDisplay();
+  
+  int motionState = digitalRead(MOTION_PIN); // Read the pin state
+
+  delay(1000);
+  Serial.println(motionState);
+
+  if (motionState == HIGH) {
+    Serial.println("MOTION DETECTED!");
+  } else {
+    koala_motion();
+  }
+
+  
 }
